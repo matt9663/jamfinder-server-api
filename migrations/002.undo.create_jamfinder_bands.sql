221a80f7c89cb8d3a61f41245ffc1bd74be2fdda
Matt Wood <matt9663@gmail.com>
2020-02-25 17:29:10 -0500

    inital commmit, basic setup and users route

---
+++ migrations/002.undo.create_jamfinder_bands.sql
@@ -0,0 +1,1 @@
+DROP IF TABLE EXISTS jamfinder_bands; 
---
+++ src/users/users-service.js
@@ -0,0 +1,49 @@
+const xss = require('xss')
+const bcrypt = require('bcryptjs')
+
+const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
+
+const UsersService = {
+  validatePassword(password) {
+    if (password.length < 8) {
+      return 'Password must be at least 8 characters'
+    }
+    if (password.length > 72) {
+      return 'Password must be no more than 72 characters'
+    }
+    if (password.startsWith(' ') || password.endsWith(' ')) {
+      return 'Password cannot start or end with empty space'
+    }
+    if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
+      return 'Password must contain at least 1 lowercase, uppercase, number, & special character'
+    }
+    return null
+  },
+  hasUserWithUserName(db, user_name) {
+    return db('jamfinder_users')
+      .where({ user_name })
+      .first()
+      .then(user => !!user)
+  },
+  insertUser(db, newUser) {
+    return db
+      .insert(newUser)
+      .into('jamfinder_users')
+      .returning('*')
+      .then(([user]) => user)
+  },
+  serializeUser(user) {
+    return {
+      id: user.id,
+      user_name: xss(user.user_name),
+      genres: xss(user.genres),
+      instrument: xss(user.instrument),
+      influences: xss(user.influences)
+    }
+  },
+  hashPassword(password) {
+    return bcrypt.hash(password, 12)
+  }
+}
+
+module.exports = UsersService
